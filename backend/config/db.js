b6f5c9cd6d66b28bce48b36f4585c8f287fd06a5
theniyazkhan <ahmadkhanniyaz@gmail.com>
2026-04-13 23:17:07 +0600

    Initial commit for CampusBite

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,9 +6,12 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    allowPublicKeyRetrieval: true,
+    ssl: { rejectUnauthorized: false }
 });
 
 const promisePool = pool.promise();
